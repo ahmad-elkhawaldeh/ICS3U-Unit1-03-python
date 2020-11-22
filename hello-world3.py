24bc20537ee33b6202b87206eec6dfc4f6ec31fb
Ubuntu <ubuntu@ip-172-31-14-246.ec2.internal>
2020-11-22 03:13:57 +0000

    python proper style

---
+++ hello-world3.py
@@ -0,0 +1,12 @@
+# !/user/bin/env python3
+
+# created by: Ahmad El-khawaldeh
+# created on: Nov 2020
+# This is "hello world " program with proper style
+
+def main():
+    print("hello world")
+    
+    
+if __name__ == "__main__":
+        main()
